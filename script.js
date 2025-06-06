ffd643db87a71307719984c6cbc43716c8e306d5
Santino Ventrice <santino.ventrice@gmail.com>
2025-06-06 02:52:44 -0300

    Creando función de submit para Contact page

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ function handleAgregarAlCarrito() {
     let numero = parseInt(carrito.textContent) || 0;
     numero += 1;
     carrito.textContent = numero;
-    alert('Producto agregado al carrito');
+}
+
+function handleSubmit() {
+    const email = document.getElementById('email').value;
+    alert(`Gracias por contactarnos. Alguien de nuestro equipo se pondrá en contacto contigo tu correo: ${email}`);
 }
