2bf2cd10fc0050ad733306b391799fca55ca2f71
Santino Ventrice <santino.ventrice@gmail.com>
2025-06-06 02:09:38 -0300

    Implementando funciones de filtro

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function handleAgregarAlCarrito() {
     numero += 1;
     carrito.textContent = numero;
 }
+
+function handleCambiarCategoría(categoria) {
+    const productos = document.querySelectorAll('.item');
+
+    productos.forEach(producto => {
+        if (producto.classList.contains(categoria) || categoria === 'all') {
+            producto.style.display = 'flex';
+        } else {
+            producto.style.display = 'none';
+        }
+    });
+}
